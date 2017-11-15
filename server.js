d7dff04bf76fdad8439771d40db887a559cf5d57
kingnt <franciswyf@hotmail.com>
2017-11-15 19:27:00 +0800

    1

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ http.createServer( function (request, response) {
    
    // 从文件系统中读取请求的文件内容
    var tar = pathname.substr(1);
-   if(tar == null) tar = 'index.html'
+   if(tar == '') tar = 'index.html'
    console.log("tar = " + tar)
    fs.readFile(tar, function (err, data) {
       if (err) {

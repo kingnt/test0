00eb0a3398ec86bb0d153bf3dd71b1496c9a8df8
kingnt <franciswyf@hotmail.com>
2017-11-15 19:22:43 +0800

    1

--- server.js
+++ server.js
@@ -6,13 +6,13 @@ var url = require('url');
 // 创建服务器
 http.createServer( function (request, response) {  
    // 解析请求，包括文件名
-   //var pathname = url.parse(request.url).pathname;
-   //if(pathname)
+   var pathname = url.parse(request.url).pathname;
+   if(pathname)
    // 输出请求的文件名
-   //console.log("Request for " + pathname + " received.");
+   console.log("Request for " + pathname + " received.");
    
    // 从文件系统中读取请求的文件内容
-   fs.readFile('index.html', function (err, data) {
+   fs.readFile(pathname.substr(1), function (err, data) {
       if (err) {
          console.log(err);
          // HTTP 状态码: 404 : NOT FOUND

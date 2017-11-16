3e1e257b9bd918f023ba79ffed62aaaaff71777f
kingnt <franciswyf@hotmail.com>
2017-11-16 13:41:21 +0800

    1

--- public/js_library/nav.js
+++ public/js_library/nav.js
@@ -1,1 +1,28 @@
-alert($('#test').height());
+var ht = new Array();
+var id = 0;
+var init = function () {
+    ht[0] = $('#head').height() - $('#nav').height();
+    id = 0;
+    $('.mydiv').each(function () {
+        ht[++id] = $(this).height();
+    });
+    for (var i = 1; i < id; i++) ht[i] += ht[i - 1];
+    //for (var i = 0; i < id; i++)
+    console.log('ht[0] = ' + ht[0]);
+    ht[id] = $(window).height();
+}
+init();
+$(window).resize(function() {
+    init();
+});
+var cnt = 0;
+$(document).scroll(function () {
+    var cur = $(document).scrollTop();
+    for(var i = 0; i < id; i++) {
+        if(cur >= ht[i] && cur < ht[i + 1]) {
+           // console.log('now at div' + i);
+           $("#pill"+i).addClass("active");
+           for(var j = 0; j < id; j++) if(j != i) $("#pill"+j).removeClass("active");
+        }
+    }
+});
